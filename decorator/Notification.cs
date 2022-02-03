d88556327cd4de1f5f1609d4cf4bed7429f1b4d3
khalil <khalillakhdharatc@gmail.com>
2022-02-03 16:23:18 +0100

    decorator

---
+++ decorator/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure.decorator
+{
+    class Notification
+    {
+        public string Text { get; set; }
+        public  string DateHeur { get; set; }
+
+        public Notification(string text, string dateHeur)
+        {
+            Text = text;
+            DateHeur = dateHeur;
+        }
+    }
+}
---
+++ decorator/SMSNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure.decorator
+{
+    class SMSNotification : SendInterface
+    {
+        public Notification notification { get; set; }
+        public void sendNotification(Notification notification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
